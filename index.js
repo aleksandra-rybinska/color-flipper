410989e483361e56e0a6cfefb763b7a9e23640ca
Aleksandra Rybinska <rybinska.aleksandra@gmail.com>
2021-02-10 20:58:14 +0100

    logic completed

---
+++ index.js
@@ -0,0 +1,35 @@
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+const value = [
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+];
+
+btn.addEventListener('click', () => {
+  let hexColor = '#';
+
+  for (i = 0; i < 6; i++) {
+    hexColor += value[getRandomValue()];
+  }
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+
+  console.log(hexColor);
+});
+function getRandomValue() {
+  return Math.floor(Math.random() * value.length);
+}
